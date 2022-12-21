3ae5e4938c99d4a8a6b1b31c9dd3a7e1c25cbe91
SodiumW <80780822+SodiumW@users.noreply.github.com>
2022-12-21 14:13:25 +0300

    working time offset

--- matlab workspace/OFDM new/coarse_time.m
+++ matlab workspace/OFDM new/coarse_time.m
@@ -1,7 +1,11 @@
 
 %% Generate OFDM Waveform
+% Burada kendi transmitter mýzdan aldýðýmýz datalarla gürültü ekleyip
+% random offset belirleyerek coarse time senkronizasyonu yapýyoruz
+
 % OFDM_transmitter
 genOFDMPacket
+
 % Add random offset
 offset = randi([0 1e2]); % simülasyon için
 y = [zeros(offset,1);y];
@@ -14,18 +18,18 @@ N = 300; % Autocorrelation samples
 M = zeros(N,1);
 SNR = 30;
 for SNRindx = 1:length(SNR)
-r = awgn(y,SNR(SNRindx),'measured');
-% Determine timing metric
-for k=1:N                   %% Auto corr döngüsü
-P = r(k:k+m)' * r(k+L:k+m+L);
-a = abs(y(k+L:k+m+L));
-R = a'*a;
-M(k) = abs(P)^2/(R^2);
-end
-% Plot
-subplot(length(SNR),1,SNRindx);stem(M);
-hold on; stem(offset+1,M(offset+1),'r*'); hold off;
-grid on;xlabel('k');ylabel('M');
-legend('Autocorrelation’,’True Start');
-title(['SNR: ',num2str(SNR(SNRindx)),'dB']);
+    r = awgn(y,SNR(SNRindx),'measured');
+    % Determine timing metric
+    for k=1:N                   %% Auto corr döngüsü
+        P = r(k:k+m)' * r(k+L:k+m+L);
+        a = abs(y(k+L:k+m+L));
+        R = a'*a;
+        M(k) = abs(P)^2/(R^2);
+    end
+    % Plot
+    subplot(length(SNR),1,SNRindx);stem(M);
+    hold on; stem(offset+1,M(offset+1),'r*'); hold off;
+    grid on;xlabel('k');ylabel('M');
+    legend('Autocorrelation’,’True Start');
+    title(['SNR: ',num2str(SNR(SNRindx)),'dB']);
 end
