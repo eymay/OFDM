01fa5224337ac2e317b0052743237d8721fd65b3
SodiumW <80780822+SodiumW@users.noreply.github.com>
2022-12-23 22:37:54 +0300

    en son datalrın elde edildiği

--- sdr/USRP_OFDM_receiver.m
+++ sdr/USRP_OFDM_receiver.m
@@ -1,38 +1,40 @@
 % function [receiveTime,overrunCount] = USRP_OFDM_receiver()
+clear
+clc
 
-Duration = 10;
-        MasterClockRate = 20e6;
-        DecimationFactor = 1;
-        SamplesPerFrame = 6640;
+Duration = 5;
+MasterClockRate = 20e6;
+DecimationFactor = 1;
+SamplesPerFrame = 1840;
 
-        SampleRate = MasterClockRate/3;
-        FrameDuration = SamplesPerFrame/SampleRate;
-        Iterations = Duration/FrameDuration;
-        
-        rx = comm.SDRuReceiver('Platform','B200','SerialNum','31FD9D5', ...
-                       'CenterFrequency', 1.2e9, ...
-                       'MasterClockRate',MasterClockRate, ...
-                       'DecimationFactor',DecimationFactor, ...
-                       'OutputDataType','double', ...
-                       'SamplesPerFrame', 6640, ...
-                       'Gain', 10);
-        count = 0;
+SampleRate = MasterClockRate/3;
+FrameDuration = SamplesPerFrame/SampleRate;
+Iterations = Duration/FrameDuration;
 
-        disp('Started Reception...');
-        tic
-        for i = 1:Iterations
-            [data,~,overrun] = rx();
-            datax(:,i) =data;
-            if overrun
-              count = count+1;
-            end  
-        end     
-       
-        
+rx = comm.SDRuReceiver('Platform','B200','SerialNum','31FD9D5', ...
+    'CenterFrequency', 1.2e9, ...
+    'MasterClockRate',MasterClockRate, ...
+    'DecimationFactor',DecimationFactor, ...
+    'OutputDataType','double', ...
+    'SamplesPerFrame', 1840, ...
+    'Gain', 20);
+count = 0;
+
+disp('Started Reception...');
+tic
+for i = 1:2500
+    [data,~,overrun] = rx();
+    datax(:,i) =data;
+    if overrun
+        count = count+1;
+    end
+end
+
+disp('Finished Reception...');
 %        USRP_OFDM_demodulation
-       
-       receiveTime = toc;
-       overrunCount = count;
-       release(rx);
+
+receiveTime = toc;
+overrunCount = count;
+release(rx);
 % end
 
