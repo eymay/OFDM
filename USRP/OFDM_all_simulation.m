af3b63e083991e291ce0a84b46806759ff90390f
Eymen Ünay <20779470+Eymay@users.noreply.github.com>
2022-12-27 13:41:58 +0300

    Update OFDM_all_simulation.m

--- USRP/OFDM_all_simulation.m
+++ USRP/OFDM_all_simulation.m
@@ -49,7 +49,7 @@ g = g/norm(g);
         x_ifft = ifft(mod_data);
         %64 symbol + 16 CP symbol = 80 symbol packets
         x_cpe(:,d) = [x_ifft(end-n_cpe+1:end,:);x_ifft];
-        %     x_cpe=awgn(x_cpe,snr,'measured');      % simülasyonlar için gürültü eklendi sonra silinecek
+        %     x_cpe=awgn(x_cpe,snr,'measured');      % simï¿½lasyonlar iï¿½in gï¿½rï¿½ltï¿½ eklendi sonra silinecek
     end
     %Adding 3 preambles codes 
     barker = comm.BarkerCode('SamplesPerFrame',80);
@@ -67,15 +67,15 @@ g = g/norm(g);
     
     
     %% Generate OFDM Waveform
-    % Burada kendi transmitter mýzdan aldýðýmýz datalarla gürültü ekleyip
-    % random offset belirleyerek coarse time senkronizasyonu yapýyoruz
+    % Burada kendi transmitter mï¿½zdan aldï¿½ï¿½ï¿½mï¿½z datalarla gï¿½rï¿½ltï¿½ ekleyip
+    % random offset belirleyerek coarse time senkronizasyonu yapï¿½yoruz
     
     SNR = 26;
 for SNRindx = 1:length(SNR)
     y = zeros(length(x_transmitted.'),1);
     y = x_transmitted.';
     % Add random offset
-    offset = randi([11 1e2]) % simülasyon için
+    offset = randi([11 1e2]) % simï¿½lasyon iï¿½in
     y = [zeros(offset,1);y];
     
     Fs =1e6;
@@ -98,7 +98,7 @@ for SNRindx = 1:length(SNR)
     % Determine timing metric
     M = zeros(N,1);
        %Used for Course Time and Course Frequency Offsets
-    for k=1:N                   %% Auto corr döngüsü
+    for k=1:N                   %% Auto corr dï¿½ngï¿½sï¿½
         P(k) = r_CFO(k:k+m,(SNRindx))' * r_CFO(k+L:k+m+L,(SNRindx));
         a = abs(y(k+L:k+m+L));
         R = a'*a;
@@ -115,44 +115,10 @@ for SNRindx = 1:length(SNR)
     title(['SNR: ',num2str(SNR(SNRindx)),'dB']);
     
     %Course Frequency Offset 
-%     a = 1;
-%     b = (1/50) * ones(1,50);
-%     diff_M = diff(M);
-%     diff_ratios = [0; M]./[M; 1];
-%        
-%     smooth_M = movmean(M, 3);
-%     diff_smooth_M = diff(smooth_M);
-%     max = 1;
-%     i = 2;
-%         while smooth_M(i + 1) >= smooth_M(i)
-%             max = i;
-%             i = i+ 1;
-%         end  
-%         interval = diff_smooth_M(i-3:i+3);
-%         diff_ratios = [0; interval]./[interval; 1];
-%         [ls,freq_i] = min(diff_ratios);
-%         freq_i = freq_i + 1;
-%         figure 
-%         stem(diff_ratios(:,SNRindx)) 
-%         if diff_smooth_M(i )/diff_smooth_M(i + 1) > 1
-%             max = i;
-%         end
-    
     starting_point_array = find(0.95<M);
     starting_point = starting_point_array(1)
     freqEst(SNRindx) = Fs/L*(angle(P(starting_point))/(2*pi));
-%     figure 
-%     stem(smooth_M(:,SNRindx))
-%     figure
-%     B = diff(smooth_M);
-%     stem(B(:,SNRindx));
-%     figure
-%     C = diff(B);
-%     C = movmean(C, 3);
-%     stem(C(:,SNRindx));
-%     figure
-%     D = diff(C);
-%     stem(abs(D(:,SNRindx)));
+
     
     %Course Frequency Compensation
     nn = 0: length(r_CFO(:,SNRindx))-1;
@@ -172,7 +138,7 @@ for SNRindx = 1:length(SNR)
     
     
     %% Fine frequency Synchronisation
-%     for ii=1:N                   %% Auto corr döngüsü
+%     for ii=1:N                   %% Auto corr dï¿½ngï¿½sï¿½
         P_fine_freq = r_compansated(idx:m+idx,SNRindx)' * r_compansated(idx+42*L:idx+42*L+m,SNRindx); %Correlation with third preamble 
         
 %     end
@@ -233,7 +199,7 @@ function [H_interpolated] = interpolate(H,pilot_loc,Nfft,method)
 % Input: H = Channel estimate using pilot sequence
 % pilot_loc = Location of pilot sequence
 % Nfft = FFT size
-% method = ’linear’/’spline’
+% method = ï¿½linearï¿½/ï¿½splineï¿½
 % Output: H_interpolated = interpolated channel
 if pilot_loc(1)>1
     slope = (H(2)-H_est(1))/(pilot_loc(2)-pilot_loc(1));
